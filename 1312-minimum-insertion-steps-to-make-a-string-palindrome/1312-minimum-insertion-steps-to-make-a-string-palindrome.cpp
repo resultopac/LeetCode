9a9a01c56dcd44814a693505c01e3ffd046bbdeb
resultopac <resultopac04@gmail.com>
2026-03-07 10:22:36 +0300

    Time: 94 ms (7.7%), Space: 48.5 MB (16.93%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+class Solution {
+public:
+
+    int helper(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        stack<pair<int,int>> st;
+        st.push({0,n-1});
+        while (!st.empty()) {
+            int i = st.top().first;
+            int j = st.top().second;
+            if (i > j) {
+                dp[i][j] = 0;
+                st.pop();
+            }else if (i == j) {
+                dp[i][j] = 1;
+                st.pop();
+            }else if (s.at(i) == s.at(j)) {
+                if (dp[i+1][j-1] == -1) {
+                    st.push({i+1,j-1});
+                }else {
+                    dp[i][j] = 2+dp[i+1][j-1];
+                    st.pop();
+                }
+            }else {
+                if (dp[i+1][j] == -1) {
+                    st.push({i+1,j});
+                    if (dp[i][j-1] == -1) {
+                        st.push({i,j-1});
+                    }
+                }
+                else if (dp[i][j-1] == -1) {
+                    st.push({i,j-1});
+                }else {
+                    dp[i][j] = max(dp[i+1][j],dp[i][j-1]);
+                    st.pop();
+                }
+            }
+        }
+        return dp[0][s.size()-1];
+    }
+
+    int minInsertions(string s) {
+        int longest = helper(s);
+        return s.size() - longest;
+        
+    }
+};
