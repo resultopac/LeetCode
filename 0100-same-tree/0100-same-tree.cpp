403098777cc7966fb3c2be88f4112230552a0c0b
resultopac <resultopac04@gmail.com>
2026-03-28 10:35:19 +0300

    Time: 0 ms (100%), Space: 13 MB (12.96%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(p);
+        q2.push(q);
+
+        while (!q1.empty()) {
+            if (q1.front() == nullptr && q2.front() == nullptr) {
+                q1.pop();
+                q2.pop();
+            }else if (q1.front() != nullptr && q2.front()!= nullptr) {
+                if (q1.front()->val != q2.front()->val) {
+                    return false;
+                }
+                q1.push(q1.front()->left);
+                q1.push(q1.front()->right);
+                q2.push(q2.front()->left);
+                q2.push(q2.front()->right);
+                q1.pop();
+                q2.pop();
+            }else{
+                return false;
+            }
+        }
+        if (q2.empty()) {
+            return true;
+        }
+        return false;
+
+    }
+};
